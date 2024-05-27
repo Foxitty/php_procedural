ff7a7818c058b16c2b5bfce6b779ec909475c252
zpt <zpt@zpt.com.br>
2024-05-27 13:22:54 -0300

    questao 4

--- src/Department.php
+++ src/Department.php
@@ -3,11 +3,35 @@ namespace Department;
 
 require_once './User.php';
 
-class Department {
+class Department
+{
 	private User\User $user;
 
-	public function __construct() {
+	public function __construct()
+	{
 		$this->user = new User\User(); // @todo fixme
+	}
+
+	public function getLargestDepartmentsForUsers()
+	{
+		$query = '
+            SELECT ud.user, d.name AS department_name, MAX(d.employees) AS max_employees
+            FROM user_department ud
+            INNER JOIN department d ON ud.department = d.id
+            GROUP BY ud.user;
+        ';
+
+		$result = $this->db->q($query);
+
+		$largestDepartments = [];
+		foreach ($result as $row) {
+			$largestDepartments[$row['user']] = [
+				'department_name' => $row['department_name'],
+				'max_employees' => $row['max_employees']
+			];
+		}
+
+		return $largestDepartments;
 	}
 }
 ?>
